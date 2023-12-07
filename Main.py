51df0945637d6c9c4377e83f4b5748b63428cc3c
GHLoic <loic.moisan@alten.com>
2023-12-07 15:30:51 +0100

    Add defence and attacks + fight managing

---
+++ Main.py
@@ -0,0 +1,39 @@
+from Soldier import Soldier
+from Player import Player
+
+""" def main():
+    
+if __name__ == 'main':
+    main() """
+
+shape = (32,32)
+
+def display_board(soldier_list1, soldier_list2):
+    for y in range(shape[0]):
+        for x in range(shape[1]):
+            print("| ", end="")
+            for soldier in range(len(soldier_list1)):
+                if soldier.x == shape.x and soldier.y == shape.y:
+                    print("X")
+            for soldier in range(len(soldier_list2)):
+                if soldier.x == shape.x and soldier.y == shape.y:
+                    print("X")
+        print("\n")
+
+player1 = Player()
+player2 = Player()
+
+for i in range(5):
+    player1.soldier_list.append(Soldier(f"soldier{+i+1}", 10, 10, 1, i+1))
+    print("p1:"+player1.soldier_list[i].name)
+
+for i in range(5):
+    player2.soldier_list.append(Soldier(f"soldier{+i+1}", 10, 10, 32, i+1))
+    print("p2:"+player2.soldier_list[i].name)
+
+player1.soldier_list[1].add_attack()
+print(player1.soldier_list[1].attack)
+
+player1.soldier_list[1].fight(player2.soldier_list[3])
+
+display_board(player1.soldier_list, player2.soldier_list)
---
+++ Player.py
@@ -0,0 +1,7 @@
+class Player:
+
+    def __init__(self):
+        start_side = ""
+        nb_point = 60
+
+    soldier_list = []
