33b7dabab95b5282e83f6fb95a0943acea4beb76
GHLoic <loic.moisan@alten.com>
2023-12-08 13:51:49 +0100

    add move_soldier method

--- Game.py
+++ Game.py
@@ -1,5 +1,6 @@
-from MapGenerator import MapGenerator
+from MapGenerator import MapGenerator, BoxType
 from Player import Player
+from Soldier import Soldier
 import random
 
 MAP_SHAPE = (32, 32)
@@ -32,3 +33,23 @@ class Game:
             soldier.y = 0
             soldier.x = 13 + i
             i += 1
+
+    def move_soldier(self, soldier: Soldier, pos_x: int, pos_y: int) -> bool:
+        # Check if pos is out of bounds
+        if pos_x < 0 or pos_y < 0 or pos_x > (MAP_SHAPE[0] - 1) or pos_y > (MAP_SHAPE[0] - 1):
+            print(f"Soldier cannot go to ({pos_x},{pos_y}): out of bound")
+            return False
+        
+        # Check if pos is a valid cell
+        if self.mapGenerator.map[pos_y][pos_x] == BoxType.Water:
+            print(f"Soldier cannot go to ({pos_x},{pos_y}): it is water")
+            return False
+        
+        # Check if pos is next to soldier
+        if abs(soldier.x - pos_x) in (0, 1) and abs(soldier.y - pos_y) in (0, 1):
+            if abs(soldier.x - pos_x) + abs(soldier.y - pos_y) > 0:
+                return True
+        
+        # If not next to the soldier
+        print(f"Soldier cannot go to ({pos_x},{pos_y}): it is not located next to the soldier")
+        return False
